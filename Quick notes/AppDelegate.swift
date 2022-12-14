e58f0e10307f13759cebcf6220f103cb2d25ef94
Nilon123456789 <dowers.purer0q@icloud.com>
2022-12-14 18:38:51 -0500

    Initial Commit

---
+++ Quick notes/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Quick notes
+//
+//  Created by Nils Lahaye on 2022-12-14.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ Quick notesUITests/Quick_notesUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Quick_notesUITestsLaunchTests.swift
+//  Quick notesUITests
+//
+//  Created by Nils Lahaye on 2022-12-14.
+//
+
+import XCTest
+
+final class Quick_notesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
