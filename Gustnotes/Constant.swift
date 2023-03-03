451dd2a0d7d8c07a186795bd0bcff8c5061e1f95
Nilon123456789 <dowers.purer0q@icloud.com>
2023-03-03 15:10:30 -0500

    MOD : keybind is now showing the correct key
    The keybind will now show for example : cmd + shift + 1 instead of cmd + shift + !

--- Gustnotes/Constant.swift
+++ Gustnotes/Constant.swift
@@ -61,7 +61,59 @@ struct GlobalKeybindPreferences: Codable, CustomStringConvertible {
             return stringBuilder
         }
         
-        if let characters = self.characters {
+        let keyMap: [UInt32: String] = [
+            0x00: "a",
+            0x01: "s",
+            0x02: "d",
+            0x03: "f",
+            0x04: "h",
+            0x05: "g",
+            0x06: "z",
+            0x07: "x",
+            0x08: "c",
+            0x09: "v",
+            0x0B: "b",
+            0x0C: "q",
+            0x0D: "w",
+            0x0E: "e",
+            0x0F: "r",
+            0x10: "y",
+            0x11: "t",
+            0x12: "1",
+            0x13: "2",
+            0x14: "3",
+            0x15: "4",
+            0x16: "6",
+            0x17: "5",
+            0x18: "=",
+            0x19: "9",
+            0x1A: "7",
+            0x1B: "-",
+            0x1C: "8",
+            0x1D: "0",
+            0x1E: "]",
+            0x1F: "o",
+            0x20: "u",
+            0x21: "[",
+            0x22: "i",
+            0x23: "p",
+            0x25: "l",
+            0x26: "j",
+            0x27: "'",
+            0x28: "k",
+            0x29: ";",
+            0x2A: "\\",
+            0x2B: ",",
+            0x2C: "/",
+            0x2D: "n",
+            0x2E: "m",
+            0x2F: "."
+        ]
+        
+        if let char = keyMap[keyCode] {
+            stringBuilder += char.uppercased()
+        }
+        else if let characters = self.characters {
             stringBuilder += characters.uppercased()
         }
         
